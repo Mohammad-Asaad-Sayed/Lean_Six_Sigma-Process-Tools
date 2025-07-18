15130d697c67e3c4aa032d966d69cd6d52d75d86
Mohd. Asaad Abrar S. <asaadsayed7@gmail.com>
2025-07-19 00:24:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lean_Six_Sigma-Process-Tools
+Lean Six Sigma Process Tools is a Python application built with Streamlit that provides tools for implementing the DMAIC methodology of Lean Six Sigma. It includes a DPMO calculator, control charts, and capability analysis, optimizing processes and supporting operational improvements across various industries.
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,121 @@
+import streamlit as st
+import sys
+import os  # Import os module for file handling
+import traceback  # Import traceback or sys to show errors
+
+# Add the src directory to the Python path
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+# System functions
+from src.auth.login import login_page
+from src.auth.register import register_page
+from src.pages.home import render_home_page
+
+# Import DPMO calculator function
+from src.tools.dpmo_calculator import dpmo_calculator_page
+
+# Import upload data function
+from src.data_management.upload import upload_data_page
+
+# Import dashboard
+from src.pages.dashboard import dashboard
+from src.tools.lss_tool1_pareto import load_lss_tool1_pareto  # Import Pareto tool
+from src.tools.lss_tool3_control_chart import load_lss_tool3_control_chart
+from src.tools.lss_hoja_verificacion import hoja_verificacion
+from src.tools.lss_tool2_ishikawa import ishikawa_page
+from src.tools.lss_histograma import histograma
+from src.tools.lss_dispersion import diagrama_dispersion
+from src.tools.lss_estratificacion import lss_estratificacion
+
+
+# Page configuration
+st.set_page_config(
+    # page_title="Analytics", 
+    page_icon=":bar_chart:", 
+    layout="wide"
+)
+
+def main():
+    # Initialize session state if it doesn't exist
+    if 'logged_in' not in st.session_state:
+        st.session_state['logged_in'] = False
+    
+    # Sidebar navigation
+    if not st.session_state['logged_in']:
+        # Menu for unauthenticated users
+        menu = st.sidebar.radio("Navigation", 
+            ["🏠 Home", "🔐 Login", "📝 Register"]
+        )
+        
+        # Render pages based on selection
+        if menu == "🏠 Home":
+            render_home_page()
+        elif menu == "🔐 Login":
+            login_page()
+        elif menu == "📝 Register":
+            register_page()
+    
+    else:
+        # Menu for authenticated users
+        menu = st.sidebar.radio("Main Menu", [
+            "🏠 Home",
+            "📝 Upload Data",
+            "📊 Dashboard",
+            "🧮 DPMO Calculator",
+            "📋 Checklist",
+            "📊 Pareto Chart",
+            "🐟 Ishikawa Diagram",
+            "📈 Histogram",
+            "🔍 Scatter Plot",
+            "🎛️ Control Charts",
+            "🔬 Stratification",  # pending
+            # "🛠️ Additional LSS Tools",
+            "🚪 Logout"
+        ])
+
+        # Navigation logic for authenticated users
+        if menu == "🏠 Home":
+            st.title(f"Welcome, {st.session_state['username']}")
+            render_home_page()
+        elif menu == "📝 Upload Data":
+            st.title("Upload Data")
+            upload_data_page()
+        elif menu == "📊 Dashboard":
+            dashboard()
+        elif menu == "🧮 DPMO Calculator":
+            try:
+                st.write("Attempting to load DPMO calculator...")
+                dpmo_calculator_page()  # Calls the dp function from dpmo_calculator.py
+            except Exception as e:
+                st.error(f"Error loading DPMO calculator: {e}")
+                st.write(f"Error details: {traceback.format_exc()}")
+        elif menu == "📋 Checklist":
+            hoja_verificacion()
+        elif menu == "📊 Pareto Chart":
+            load_lss_tool1_pareto()
+        elif menu == "🐟 Ishikawa Diagram":
+            ishikawa_page()
+        elif menu == "📈 Histogram":
+            histograma()
+        elif menu == "🔍 Scatter Plot":
+            diagrama_dispersion()
+        elif menu == "🔬 Stratification":
+            lss_estratificacion.ejecutar_modulo()
+        elif menu == ".panelControl Charts":
+            load_lss_tool3_control_chart()
+        elif menu == "🚪 Logout":
+            # Logout
+            st.session_state['logged_in'] = False
+            st.session_state['username'] = None
+            st.experimental_rerun()
+
+
+# Custom styles
+def load_css():
+    with open("static/css/styles.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# Main entry point
+if __name__ == "__main__":
+    load_css()  # Load custom styles
+    main()
