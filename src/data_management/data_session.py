468f725eddccda817c82b68316f9d40293341e04
Mohd. Asaad Abrar S. <asaadsayed7@gmail.com>
2025-07-19 00:30:34 +0530

    Update data_session.py

--- src/data_management/data_session.py
+++ src/data_management/data_session.py
@@ -4,22 +4,21 @@ import pandas as pd
 class DataSession:
     @staticmethod
     def set_dataframe(df):
-        """Almacenar DataFrame en sesión de Streamlit"""
+        """Store DataFrame in Streamlit session state"""
         st.session_state['shared_dataframe'] = df
-
 
     @staticmethod
     def get_dataframe():
-        """Recuperar DataFrame de sesión de Streamlit"""
+        """Retrieve DataFrame from Streamlit session state"""
         return st.session_state.get('uploaded_data', None)
     
     # @staticmethod
     # def get_dataframe():
-    #     """Recuperar DataFrame de sesión de Streamlit"""
+    #     """Retrieve DataFrame from Streamlit session state"""
     #     return st.session_state.get('shared_dataframe', None)
     
     @staticmethod
     def clear_dataframe():
-        """Limpiar DataFrame de sesión"""
+        """Clear DataFrame from session state"""
         if 'shared_dataframe' in st.session_state:
             del st.session_state['shared_dataframe']
